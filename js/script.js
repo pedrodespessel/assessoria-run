299ac6e8408c24c84fc2b99daabc7f777c99ddcf
pedrodespessel <despesselp@gmail.com>
2022-07-06 16:57:25 -0300

    v1.1
    Adição de pontuação, sons e alguns ajustes na tela.

--- js/script.js
+++ js/script.js
@@ -2,14 +2,55 @@ const benchimol = document.querySelector('.benchimol');
 const meias = document.querySelector('.meias');
 const gameover = document.querySelector('.game-over');
 
+var scorejs = 0
+
+//audios
+
+const point = new Audio(); 
+point.src = "./songs/point.mp3";
+
+const ui = new Audio();
+ui.src = "./songs/ui.mp3";
+
+const pulo = new Audio();
+pulo.src = "./songs/pulo.wav"
+
+const cavalo = new Audio();
+cavalo.src = "./songs/cavalo.mp3"
+
+const hit = new Audio();
+hit.src = "./songs/hit.wav"
+
+
+
+
+window.onload = function start () {
+    interval1 = setInterval(counter,2000);
+}
+
+function counter (){
+    scorejs++
+    ui.play();
+    document.getElementById('score').innerText=scorejs;
+
+    if (scorejs == 10) { 
+        cavalo.play();
+        ui.pause();
+    }
+}
+
+
+
 const jump = () => {
     benchimol.classList.add('jump');
 
     setTimeout(() => {
         benchimol.classList.remove('jump');
     }, 700);
+    pulo.play();
 
 }
+
 const loop = setInterval(() => {
 
     const meiasPosition = meias.offsetLeft;
@@ -19,17 +60,17 @@ const loop = setInterval(() => {
 
     //se perder...
     if (meiasPosition <= 45 && meiasPosition > 0 && benchimolPosition < 60) {
-
         meias.style.animation = 'none';
         meias.style.left = `${meiasPosition}px`;
-
         benchimol.style.animation = 'none';
         benchimol.style.bottom = `${benchimolPosition}px`;
-
+        clearInterval(interval1);
         gameover.style.display = 'block'; 
     }
 
 }, 10);
+
+
 
 document.addEventListener('keydown', jump);
 document.addEventListener('click', jump)
